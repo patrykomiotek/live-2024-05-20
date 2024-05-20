bd5e54d7ef4e4b09412786eb79f657e1b0d53b49
Patryk Omiotek <patryk.omiotek@gmail.com>
2024-05-20 17:29:37 +0200

    feat: with prisma

---
+++ libs/prisma-schema/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/libs/prisma-schema',
+
+  plugins: [nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  test: {
+    globals: true,
+    cache: { dir: '../../node_modules/.vitest/libs/prisma-schema' },
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/libs/prisma-schema',
+      provider: 'v8',
+    },
+  },
+});
